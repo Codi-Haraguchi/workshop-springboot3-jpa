980e3ab5db22adddf088bbae26b02530c9b28b82
Codi <codi798@gmail.com>
2022-12-03 18:29:28 -0300

    User delete

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -22,6 +22,10 @@ public class UserService {
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
 
 	public User findById(Long id) {
 		// TODO Auto-generated method stub
