9d1d8c7506d2a7819f3e9e6da2d5533b1a45342a
Codi <codi798@gmail.com>
2022-12-07 16:14:09 -0300

    findById

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -1,22 +1,29 @@
 package com.educandoweb.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
+
 
 @Service
 public class UserService {
 	
 	@Autowired
 	private UserRepository repository;
-	public User findById;
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById1(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
